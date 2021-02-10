787d777f4580bac293c3d6a7c10f09da2eb32ce1
Andre <andre.fabian.melo@gmail.com>
2021-02-10 12:34:05 -0300

    Add Heading line color and size addons

--- src/components/Menu/stories.tsx
+++ src/components/Menu/stories.tsx
@@ -1,16 +1,12 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import Menu from '.'
+import Menu, { MenuProps } from '.'
 
 export default {
   title: 'Menu',
-  component: Menu,
-  args: {
-    title: 'React Avançado',
-    description: 'Typescript, NextJS e Styled Components'
-  }
+  component: Menu
 } as Meta
 
-export const Default: Story = () => <Menu />
+export const Default: Story<MenuProps> = args => <Menu {...args} />
 
 Default.parameters = {
   layout: 'fullscreen',

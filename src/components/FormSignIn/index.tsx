8ab4071d54d4894e692386f9c8c737741e538b42
Andre <andre.fabian.melo@gmail.com>
2021-02-17 17:07:44 -0300

    Add FormSignUp full component

--- src/components/FormSignIn/index.tsx
+++ src/components/FormSignIn/index.tsx
@@ -1,11 +1,13 @@
 import Link from 'next/link'
 import { Email, Lock } from '@styled-icons/material-outlined'
+
+import { FormLink, FormWrapper } from 'components/Form'
 import Button from 'components/Button'
 import TextField from 'components/TextField'
 import * as S from './styles'
 
 const FormSignIn = () => (
-  <S.Wrapper>
+  <FormWrapper>
     <form>
       <TextField
         name="email"
@@ -25,14 +27,14 @@ const FormSignIn = () => (
         Sign in now
       </Button>
 
-      <S.FormLink>
+      <FormLink>
         Don’t have an account?{' '}
         <Link href="/signup">
           <a>Sign up</a>
         </Link>
-      </S.FormLink>
+      </FormLink>
     </form>
-  </S.Wrapper>
+  </FormWrapper>
 )
 
 export default FormSignIn

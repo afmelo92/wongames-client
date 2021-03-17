4aaa91492838aa9636fa88d0df0f79cdfbfc03cc
Andre <andre.fabian.melo@gmail.com>
2021-03-17 12:13:55 -0300

    Update Home page with dynamic date query

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Home, { HomeTemplateProps } from 'templates/Home'
 import { initializeApollo } from 'utils/apollo'
-import { QueryHome } from 'graphql/generated/QueryHome'
+import { QueryHome, QueryHomeVariables } from 'graphql/generated/QueryHome'
 import { QUERY_HOME } from 'graphql/queries/home'
 import { GetStaticProps } from 'next'
 import { bannerMapper, gamesMapper, highlightMapper } from 'utils/mappers'
@@ -11,16 +11,20 @@ export default function Index(props: HomeTemplateProps) {
 
 export const getStaticProps: GetStaticProps = async () => {
   const apolloClient = initializeApollo()
+  const TODAY = new Date().toISOString().slice(0, 10)
 
   const {
     data: { banners, newGames, upcomingGames, freeGames, sections }
-  } = await apolloClient.query<QueryHome>({ query: QUERY_HOME })
+  } = await apolloClient.query<QueryHome, QueryHomeVariables>({
+    query: QUERY_HOME,
+    variables: { date: TODAY }
+  })
 
   return {
     props: {
-      revalidate: 60,
+      revalidate: 10,
       banners: bannerMapper(banners),
-      newGameTitle: sections?.newGames?.title,
+      newGamesTitle: sections?.newGames?.title,
       newGames: gamesMapper(newGames),
       mostPopularGamesTitle: sections?.popularGames?.title,
       mostPopularGames: gamesMapper(sections!.popularGames!.games),

40ad930dbfc3c8194411c2e0eb51c9aa53a20577
Andre <andre.fabian.melo@gmail.com>
2021-03-26 23:04:39 -0300

    Update profile page data fetch with authorization

--- src/pages/profile/me.tsx
+++ src/pages/profile/me.tsx
@@ -1,22 +1,30 @@
-import FormProfile from 'components/FormProfile'
+import FormProfile, { FormProfileProps } from 'components/FormProfile'
+import { QueryProfileMe } from 'graphql/generated/QueryProfileMe'
+import { QUERY_PROFILE_ME } from 'graphql/queries/profile'
 import { GetServerSidePropsContext } from 'next'
 import Profile from 'templates/Profile'
+import { initializeApollo } from 'utils/apollo'
 import protectedRoutes from 'utils/protected-routes'
 
 // import { Container } from './styles';
 
-export default function ProfileMePage() {
+export default function ProfileMePage(props: FormProfileProps) {
   return (
     <Profile>
-      <FormProfile />
+      <FormProfile {...props} />
     </Profile>
   )
 }
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   const session = await protectedRoutes(context)
+  const apolloClient = initializeApollo(null, session)
+
+  const { data } = await apolloClient.query<QueryProfileMe>({
+    query: QUERY_PROFILE_ME
+  })
 
   return {
-    props: { session }
+    props: { session, username: data.me?.username, email: data.me?.email }
   }
 }
